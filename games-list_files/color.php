1e6c6474bf61b7ab2c87cf6490aeb76bff647e0a
metal-headsallmusic <54188951+metal-headsallmusic@users.noreply.github.com>
2025-01-20 02:46:49 +0530

    Add files via upload

---
+++ games-list_files/color.php
@@ -0,0 +1,21 @@
+
+.home-3 .navbar-area .nav-container .navbar-collapse .navbar-nav li:hover a,
+.breadcrumb-area .page-list,
+.blog-share-area ul li:hover a,
+.widget_catagory ul li a:hover,
+.blog-details-page-content .single-blog-inner .details .blog-meta li i, .blog-details-page-content .single-blog-inner .details .blog-meta li svg,
+.text--base,
+a:hover,
+.navbar-top ul li a:hover,
+.navbar-top ul li span, .navbar-top ul li i, .navbar-top ul li svg{
+    color : #746EF1 !important;
+}
+
+.home-3 .navbar-area .nav-container .navbar-collapse .navbar-nav li:hover a:after,
+.home-3 .navbar-area .nav-container .navbar-collapse .navbar-nav li.menu-item-has-children .sub-menu li:hover,
+.home-3 .navbar-area .nav-container .navbar-collapse .navbar-nav li:hover::before, .home-3 .navbar-area .nav-container .navbar-collapse .navbar-nav li:hover::after,
+.footer-area-3 .single-subscribe-inner .btn,
+.single-blog-inner-2 .details .read-more-text::after,
+.back-to-top{
+    background : #746EF1 !important;
+}
